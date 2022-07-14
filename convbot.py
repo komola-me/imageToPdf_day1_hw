47fb7639755555a1131efc763ca642a675c093dc
komola-me <komolaziya28@gmail.com>
2022-07-14 14:06:40 +0500

    feat: convert image file to pdf

--- convbot.py
+++ convbot.py
@@ -1,60 +1,117 @@
-from email.contentmanager import ContentManager
-from turtle import update
-import convertapi
-import telebot
-import tempfile
-import requests
-from io import BytesIO
+#!/usr/bin/env python
+# pylint: disable=C0116,W0613
 
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from email import message
+from importlib.resources import path
+from inspect import getfile
+from io import BytesIO
+import logging
+from msilib.schema import File
+from os import name
+import os
+from pydoc import Doc, doc
+import tempfile
+from tkinter import PhotoImage
+from urllib import request
+from xml.dom.minidom import Document
+import requests
+from telegram import Update, ForceReply, ReplyKeyboardMarkup, File
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+import convertapi
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
 
 token = '5423620003:AAFpTH-Ruzs4JTk9sKWHiFwKWhANDYxZWg4'
 convertapi.api_secret = 'bELJODOJ9siIZspR'
 
-bot = telebot.TeleBot(token)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+def make_keyboard_for_start_command() -> ReplyKeyboardMarkup:
+    buttons = [
+        ['Rasm to PDF'],
+        {'Aloqa', 'Manzil'},
+        ['About']
+    ]
+
+    return ReplyKeyboardMarkup(buttons)
+
+def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
-    await update.message.reply_markdown_v2(
-        f'Hi {user.mention_markdown_v2()}\! \n\n /convertToPdf \- to convert your files to pdf \n'
-     )
+    update.message.reply_text(
+        f'Welcome, {user.first_name} \n', reply_markup=make_keyboard_for_start_command()
+    )
 
-async def convert_to_pdf(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    user = update.effective_user
-    await update.message.reply_markdown_v2(
-        f'Please send your file here'
-     )
+def receive_image(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Please send your photo in file form.')
 
-async def converting(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    user = update.effective_user
-    bot.get_file()
+def img_to_pdf(update: Update, context: CallbackContext) -> None:
+    file = context.bot.get_file(update.message.document.file_id)
+    #print(file)
+    file_data = BytesIO(requests.get(f'{file.file_path}').content)
+    #print(file_data)
+    # file name
+    file_name = update.message.document.file_name
+    update.message.reply_text('Wait.. Converting..')
+    upload = convertapi.UploadIO(file_data, filename=file_name)
+    convert = convertapi.convert('pdf', {'File': upload})
+    result = convert.save_files(tempfile.gettempdir())
 
-    await update.message.reply_markdown_v2(
-        f'Wait. The file is being converted!'
-     )
+    #update.message.reply_document(document='https://andonovicmilica.files.wordpress.com/2018/07/short-stories-for-children.pdf')
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    for f in result:
+        context.bot.send_document(update.message.chat.id, open(f, 'rb'))
+
+def aloqa(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Savol va takliflar uchun komolaziya28@gmail.com maniziliga e-mail yuborishingiz mumkin.')
+
+def about(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Bu bot orqali xohlagan rasmingizni pdf formatiga o\'tkazishingiz mumkin')
+
+def manzil(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Bizning manzil: ')
+    update.message.reply_location(longitude=49.779298, latitude=42.885564)
+
+
+def echo(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
-    await update.message.reply_text(update.message.text)
+    update.message.reply_text(update.message.text)
 
-  
 
 def main() -> None:
     """Start the bot."""
-    # Create the Application and pass it your bot's token.
-    application = Application.builder().token("5423620003:AAFpTH-Ruzs4JTk9sKWHiFwKWhANDYxZWg4").build()
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(token)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
 
     # on different commands - answer in Telegram
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("convertToPdf", convert_to_pdf))
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.text("Aloqa"), aloqa))
+    dispatcher.add_handler(MessageHandler(Filters.text("About"), about))
+    dispatcher.add_handler(MessageHandler(Filters.text("Manzil"), manzil))
+    dispatcher.add_handler(MessageHandler(Filters.text("Rasm to PDF"), receive_image))
+    dispatcher.add_handler(MessageHandler(Filters.document, img_to_pdf))
     # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-    application.add_handler(MessageHandler(filters.PHOTO, converting))
-    # Run the bot until the user presses Ctrl-C
-    application.run_polling()
+    dispatcher.add_handler(MessageHandler(
+        Filters.text & ~Filters.command, echo))
 
 
-if __name__ == "__main__":
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
     main()
